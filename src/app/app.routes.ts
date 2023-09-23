67411d56bf3883173744e687a86a835d4efaa065
i.bulanov <i.bulanov@concert.ua>
2023-09-23 03:21:26 +0300

    Made some preparations for development Added mocked backend (emulation) with localStorage as DB Also set up routing with named outlets

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import {Routes} from "@angular/router";
+import {UserListComponent} from "./components/user-list/user-list.component";
+import {userResolver} from "./shared/resolvers/user.resolver";
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '**',
+    redirectTo: ''
+  },
+  {
+    path: '',
+    loadComponent: () => import('./components/user-list/user-list.component').then(m => m.UserListComponent)
+  },
+  {
+    path: 'user',
+    loadComponent: () => import('./components/user-form/user-form.component').then(m => m.UserFormComponent),
+    outlet: 'userForm'
+  },
+  {
+    path: 'user/:id',
+    loadComponent: () => import('./components/user-form/user-form.component').then(m => m.UserFormComponent),
+    outlet: 'userForm',
+    resolve: {user: userResolver}
+  }
+  ]
