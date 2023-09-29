2d65ee9756bc1e35c571ea1f9a166ded06cabc9d
i.bulanov <i.bulanov@concert.ua>
2023-09-29 10:13:38 +0300

    Minor fixes

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -11,10 +11,22 @@ import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 })
 export class NotFoundComponent implements OnInit{
   errorCode: string = '404'
-  constructor(private route: ActivatedRoute) {}
+  errorMessage: string = 'Page not found'
+  routeParams: any
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.routeParams = this.router.getCurrentNavigation()
+  }
 
   ngOnInit() {
-    console.log(this.route.snapshot.queryParamMap.get('type'))
-    this.errorCode = this.route.snapshot.queryParamMap.get('type') || '404'
+    console.log(this.routeParams)
+    console.log(history.state)
+    this.route.params.subscribe(paramsData => {
+      console.log(paramsData)
+    })
+    console.log(this.route.snapshot.queryParamMap)
+    console.log(this.route.queryParams)
+    console.log(this.router.getCurrentNavigation()!.extras.state)
+    // this.errorCode = this.route.snapshot.queryParamMap.get('type') || '404'
+    // this.errorCode = this.route.snapshot.queryParamMap.get('message') || '404'
   }
 }
